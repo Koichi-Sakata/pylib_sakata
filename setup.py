29884f91faa107e0868f299f160f64209527e07d
Koichi Sakata <sakata0330@gmail.com>
2021-08-14 22:14:34 +0900

    setup.py is modified.

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@
 
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="pylib_sakata",
     version="0.0.1",
-    author="Example Author",
+    author="Koichi Sakata",
     author_email="",
     description="Control system design and analysis package",
-    url="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Koichi-Sakata/pylib_sakata",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
