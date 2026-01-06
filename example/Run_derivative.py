2158a4a410a0670c12147c645d9cbda4e6aa62d0
Koichi-Sakata <sakata0330@gmail.com>
2026-01-06 23:55:58 +0900

    v-0.3.8

---
+++ example/Run_derivative.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2021 Koichi Sakata
+
+
+from pylib_sakata import init as init
+# uncomment the follows when the file is executed in a Python console.
+# init.close_all()
+# init.clear_all()
+
+import os
+import shutil
+import numpy as np
+from pylib_sakata import ctrl
+from pylib_sakata import plot
+
+print('Start simulation!')
+
+# Common parameters
+figurefolderName = 'figure_derivative'
+if os.path.exists(figurefolderName):
+    shutil.rmtree(figurefolderName)
+os.makedirs(figurefolderName)
+dataNum = 10000
+freqrange = [1, 10000]
+freq = np.logspace(np.log10(freqrange[0]), np.log10(freqrange[1]), dataNum, base=10)
+print('Common parameters were set.')
+
+# Plant model
+tau = 1/(2.0 * np.pi * 1000)
+Exact_differential  = ctrl.tf([1, 0], [1])
+Quasi_derivative = ctrl.tf([1, 0], [tau, 1])
+Exact_differential_frd = ctrl.sys2frd(Exact_differential, freq)
+Quasi_derivative_frd = ctrl.sys2frd(Quasi_derivative, freq)
+print('Plant model was set.')
+
+print('Plotting figures...')
+# Plant
+fig = plot.makefig()
+ax_mag = fig.add_subplot(211)
+ax_phase = fig.add_subplot(212)
+plot.plot_tffrd(ax_mag, ax_phase, Exact_differential_frd, '-', 'b', 1.5, 1.0, title='Frequency response of derivative')
+plot.plot_tffrd(ax_mag, ax_phase, Quasi_derivative_frd, '--', 'm', 1.5, 1.0, magrange=[0, 100], legend=['Exact differential', 'Quasi-derivative'])
+plot.savefig(figurefolderName+'/freq_derivative.png')
+
+plot.showfig()
+print('Finished.')
